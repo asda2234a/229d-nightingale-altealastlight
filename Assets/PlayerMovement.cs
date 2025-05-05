af4a5804961df4fe5485687727b0574390f2a28f
ZseekI <kongpop.thong@bumail.net>
2025-05-05 22:26:14 +0700

    Start

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpForce = 150f;
+    public bool isJumping = false;
+
+    private float moveInput;
+    private Rigidbody2D rb2d;
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput = Input.GetAxis("Horizontal");
+
+        // เคลื่อนที่ซ้าย-ขวา
+        rb2d.linearVelocity = new Vector2(moveInput * speed, rb2d.linearVelocity.y);
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            rb2d.AddForce(new Vector2(rb2d.linearVelocity.x, jumpForce));
+
+        }
+
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+        }
+    }//OnCollisionEnter2D
+    private void OnCollisionExit2D(Collision2D other)
+
+    {
+
+        if (other.gameObject.CompareTag("Ground"))
+
+        {
+
+            isJumping = true;
+
+        }
+
+    }//OnCollisionExit2D
+
+}//playerMovement
+
+
+
+
