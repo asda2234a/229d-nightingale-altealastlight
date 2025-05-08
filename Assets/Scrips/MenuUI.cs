756c7b10c391f7ce75da58e1da7b321909ee0019
ZseekI <kongpop.thong@bumail.net>
2025-05-09 00:29:04 +0700

    Altea Last Light Protype 1.3

--- Assets/Scrips/MenuUI.cs
+++ Assets/Scrips/MenuUI.cs
@@ -3,20 +3,22 @@ using UnityEngine.SceneManagement;
 
 public class MenuUI : MonoBehaviour
 {
+    public GameObject creditUi;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void StartGame()
     {
         SceneManager.LoadScene("Play");
+        creditUi.SetActive(false);
     }
 
     public void Credit()
     {
-
+        creditUi.SetActive(true);
     }
 
     public void ExitCredit()
     {
-        
+        creditUi.SetActive(false);
     }
 
 }
